c40202b5a7a776fec09c4e26344a87033faafd07
Shishi <andreiusita6@gmail.com>
2025-11-04 19:21:33 +0800

    Add feat to add name in match

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ const gameBoard = (function () {
 function createPlayer(name, side) {
 	return { name, side };
 }
+
+function setGame() {
+	const role = {
+		x: 'X',
+		o: 'O',
+	}
+
+	function makeMatch () {
+		let name1 = prompt('1');
+		let playerOne = createPlayer(name1, role.x);
+
+		let name2 = prompt('2');
+		let playerTwo = createPlayer(name2, role.o);
+
+		return { playerOne, playerTwo };
+	}
+
+	return { makeMatch };
+}
